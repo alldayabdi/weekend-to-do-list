8523a161e8baa38005086809143d2ba8728eb43d
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2022-02-11 15:58:17 -0600

    Built object to hold submitted value and tested it through console log

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,24 @@ $(document).ready(readyNow)
 
 function readyNow(){
     console.log('JQuery is working!!');
+$('#submitButton').on('click', submitTask);
+}
+
+function submitTask(){
+    console.log('Testing Submit Button');
+        let taskObject = {
+            task: $('#task').val(),
+        }
+        console.log(taskObject);
+        
+        $.ajax({
+            type: 'POST',
+            url: '/tasks',
+            data: taskObject
+        }).then( function (response) {
+            $('#task').val(''),
+            getTasks();
+        });
+    
     
 }
